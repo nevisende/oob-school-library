f200fb5881f433017489d8d83b5464e7522eec90
nevisende <ekonomitakipmail@gmail.com>
2022-03-01 23:48:20 +0300

    Update: person class

--- person.rb
+++ person.rb
@@ -1,29 +1,19 @@
 class Person
-  attr_reader :id, name, age
-  attr_writer :name, :age
-  def initialize(name: "Unknown", age, parent_permission: true)
+  attr_accessor :name, :age
+  attr_reader :id
+  def initialize(age ,name =  'Unknown', parent_permission: true)
+    @id = Random.rand(1..500)
     @name = name
     @age = age
     @parent_permission = parent_permission
   end
 
-  def is_of_age
-    if @age > 18 
-      true
-    else
-      false
-    end
+  def is_of_age?
+    @age >= 18 
   end
-  private :is_of_age
+  private :is_of_age?
 
-  def can_use_services
-    if @age > 18 && parent_permission
-      true
-    else
-      false
-    end
+  def can_use_services?
+    is_of_age? || @parent_permission
   end
 end
-
-furkan = Person.new("furkan",23)
-p furkan

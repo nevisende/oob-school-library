110af8ac3a8f339254cc678caf63e195a3ed51fd
Anselem <anselemodims@gmail.com>
2022-03-09 11:40:19 +0100

    Test: write test to throw error if the Person class is initialized without arguments

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -12,6 +12,12 @@ describe Person do
     end
   end
 
+  context 'instance check' do
+    it 'should throw error if it is initialized without arguments' do
+      expect{ Person.new }.to raise_exception(ArgumentError)
+    end
+  end
+
   context 'instance varaible accessibility' do
     it 'should be able to read the age property' do
       expect(@person.age).to eq 30
@@ -55,6 +61,12 @@ describe Person do
     it 'should not be able to write to the rentals' do
       expect{ @person.rentals = [1,2,3] }.to raise_exception(NoMethodError)
     end
+
+    it 'should not be able to read or write to the parent_permission' do
+      expect{ @person.parent_permission = false }.to raise_exception(NoMethodError)
+    end
   end
+
+
 
 end
